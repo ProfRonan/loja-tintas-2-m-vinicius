3d3bb835dc77b60a13cfc7bf5440a94631f2d751
Marcus Vinicius <marcusvinicius977.mvo@gmail.com>
2020-12-14 12:57:36 +0000

    Olá professor Ainda não consegui fazer o exercício rodar sem erros. Não consigo resolver essa parte.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     a = float(input('> '))
 
     quant_litros_latas = a / 6
-    latas = quant_litros_latas / 18
+    latas = (quant_litros_latas / 18) * 1.1
     latas = math.ceil(latas)
     preco_latas = latas * 80
 
@@ -18,14 +18,24 @@ def main():
     print(f'Serão necessárias {latas} latas.')
 
     quant_litros_galao = a / 6
-    galao = quant_litros_galao / 3.6
+    galao = (quant_litros_galao / 3.6) * 1.1
     galao = math.ceil(galao)
     preco_galao = galao * 25
 
-    print(f'O valor gasto comprando apenas latas é de R$ {preco_galao:.2f}.')
+    print(f'O valor gasto comprando apenas galões é de R$ {preco_galao:.2f}.')
     print(f'Serão necessárias {galao} latas.')
 
     #O valor gasto comprando de forma que gere a menor quantidade de desperdício é de R$ [valor com duas casas decimais].Serão necessárias [quantidade] latas e [quantidade] galões.
+
+    litros = ((quant_litros_latas * 1.1) // 18)
+    resto = (litros % 18)
+    litros = math.ceil(litros)
+    resto = math.ceil(resto)
+    valor_final = (litros * 80) + ((resto) * 25)
+
+    print(
+        f'O valor gasto comprando de forma que gere a menor quantidade de desperdício é de R$ {valor_final:.2f}. Serão necessárias {litros} latas e {resto} galões.'
+    )
 
 
 if __name__ == '__main__':
